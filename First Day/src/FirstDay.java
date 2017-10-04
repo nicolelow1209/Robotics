95b529529a1f99897289e3ad52f854b179d69c5e
nicolelow1209 <31107038+nicolelow1209@users.noreply.github.com>
2017-10-04 16:51:18 -0700

    hello

---
+++ First Day/src/FirstDay.java
@@ -0,0 +1,16 @@
+//This method prints out 5 variables
+public class FirstDay {
+	public static void main (String [] args){
+		int x = 1;
+		double y = 2.0;
+		char z = 'z';
+		boolean p = true;
+		String q = "q";
+	
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		System.out.println(p);
+		System.out.println(q);
+	}
+}
