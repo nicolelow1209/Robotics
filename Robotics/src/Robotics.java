f5250b0261abbf1245f5c292dfdb560dac93f133
nicolelow1209 <31107038+nicolelow1209@users.noreply.github.com>
2017-10-04 16:06:23 -0700

    hello

---
+++ Robotics/src/Robotics.java
@@ -0,0 +1,8 @@
+//This method prints out ILoveRobotics!
+public class Robotics {
+	public static void main(String[] args) {
+		System.out.print("I");
+		System.out.print("Love");
+		System.out.print("Robotics!");
+	}
+}
